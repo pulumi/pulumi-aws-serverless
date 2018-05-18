2db5550f7b8678f8cfafe50b723b44924bacba18
Cyrus Najmabadi <cyrus@pulumi.com>
2018-05-18 12:01:33 -0700

    Initial population ofthe serverless repo.

---
+++ README.md
@@ -0,0 +1,13 @@
+[![Build Status](https://travis-ci.com/pulumi/pulumi-aws-serverless.svg?token=eHg7Zp5zdDDJfTjY8ejq&branch=master)](https://travis-ci.com/pulumi/pulumi-aws-serverless)
+
+# Pulumi Amazon Web Services (AWS) Serverless Components
+
+Pulumi's framework for useful serverless components
+
+
+# Changelog
+
+## Unreleased
+
+## Released
+
---
+++ nodejs/aws-serverless/utils.ts
@@ -0,0 +1,16 @@
+// Copyright 2016-2017, Pulumi Corporation.  All rights reserved.
+
+import * as crypto from "crypto";
+
+// sha1hash returns a partial SHA1 hash of the input string.
+export function sha1hash(s: string): string {
+    const shasum: crypto.Hash = crypto.createHash("sha1");
+    shasum.update(s);
+    // TODO[pulumi/pulumi#377] Workaround for issue with long names not generating per-deplioyment randomness, leading
+    //     to collisions.  For now, limit the size of hashes to ensure we generate shorter/ resource names.
+    return shasum.digest("hex").substring(0, 8);
+}
+
+// tslint:disable-next-line:max-line-length
+export type Diff<T extends string, U extends string> = ({[P in T]: P } & {[P in U]: never } & { [x: string]: never })[T];
+export type Omit<T, K extends keyof T> = Pick<T, Diff<keyof T, K>>;

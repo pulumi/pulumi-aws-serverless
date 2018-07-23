3e13b420d50929a80248d78c9623c0649080fd82
Cyrus Najmabadi <cyrus@pulumi.com>
2018-07-23 14:35:10 -0700

    Add more functionality to the core API class.

--- nodejs/aws-serverless/api.ts
+++ nodejs/aws-serverless/api.ts
@@ -12,6 +12,10 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+import * as fs from "fs";
+import * as mime from "mime";
+import * as fspath from "path";
+
 import * as aws from "@pulumi/aws";
 import * as pulumi from "@pulumi/pulumi";
 
@@ -71,20 +75,40 @@ export type Route = {
     handler: RouteHandler;
 };
 
+// StaticRoute is a registered static file route, backed by an S3 bucket.
+export interface StaticRoute {
+    path: string;
+    localPath: string;
+
+    /**
+     * The `content-type` to serve the file as.  Only valid when localPath points to a file.  If
+     * localPath points to a directory, the content types for all files will be inferred.
+     */
+    contentType?: string;
+
+    /**
+     * By default API.static will also serve 'index.html' in response to a request on a directory.
+     * To disable this set false or to supply a new index pass an appropriate name.
+     */
+    index?: boolean | string;
+}
+
 export interface APIArgs {
     /**
-     * Routes to use to initialize the APIGateway.
+     * The full specification to use to create the API.
      *
-     * Either [routes] or [swaggerSpec] must be specified.
+     * Either [swaggerSpec] or [swaggerString] must be specified.
      */
-    routes?: Route[];
+    swaggerSpec?: SwaggerSpec;
+
     /**
-     * A SwaggerSpec to use to initialize the APIGateway.  Note that you must manually provide permission for any route
-     * targets to be invoked by API Gateway when using [swaggerSpec].
+     * A Swagger specification already in string form to use to initialize the APIGateway.  Note
+     * that you must manually provide permission for any route targets to be invoked by API Gateway
+     * when using [swaggerString].
      *
-     * Either [routes] or [swaggerSpec] must be specified.
+     * Either [swaggerSpec] or [swaggerString] must be specified.
      */
-    swaggerSpec?: pulumi.Input<string>;
+    swaggerString?: pulumi.Input<string>;
 
     stageName?: pulumi.Input<string>;
 }
@@ -101,17 +125,13 @@ export class API extends pulumi.ComponentResource {
 
         let swaggerString: pulumi.Output<string>;
         let swaggerSpec: SwaggerSpec | undefined;
-        let lambdas: { [key: string]: aws.lambda.Function };
-        if (args.swaggerSpec) {
-            swaggerString = pulumi.output(args.swaggerSpec);
-            lambdas = {};
-        } else if (args.routes) {
-            const [spec, routeLambdas] = swaggerSpecFromRoutes(name, args.routes);
-            swaggerSpec = spec;
-            swaggerString = createSwaggerString(spec);
-            lambdas = routeLambdas;
+        if (args.swaggerString) {
+            swaggerString = pulumi.output(args.swaggerString);
+        } else if (args.swaggerSpec) {
+            swaggerSpec = args.swaggerSpec;
+            swaggerString = createSwaggerString(swaggerSpec);
         } else {
-            throw new Error("RestAPI must specify either `routes` or `swaggerSpec` options to configure the RestAPI.");
+            throw new Error("API must specify either `swaggerString` or `swaggerSpec` options.");
         }
 
         const stageName = args.stageName || "stage";
@@ -145,8 +165,9 @@ export class API extends pulumi.ComponentResource {
         if (swaggerSpec) {
             for (const path of Object.keys(swaggerSpec.paths)) {
                 for (let method of Object.keys(swaggerSpec.paths[path])) {
-                    const methodAndPath = method + ":" + path;
-                    const lambda = lambdas[methodAndPath];
+                    const operation = swaggerSpec.paths[path][method];
+                    const lambda = operation.lambda;
+
                     if (lambda) {
                         if (method === "x-amazon-apigateway-any-method") {
                             method = "*";
@@ -154,6 +175,8 @@ export class API extends pulumi.ComponentResource {
                         else {
                             method = method.toUpperCase();
                         }
+
+                        const methodAndPath = method + ":" + path;
                         const permissionName = name + "-" + sha1hash(methodAndPath);
                         const invokePermission = new aws.lambda.Permission(permissionName, {
                             action: "lambda:invokeFunction",
@@ -200,6 +223,9 @@ interface SwaggerOperationAsync {
     parameters?: any[];
     responses?: { [code: string]: SwaggerResponse };
     "x-amazon-apigateway-integration": ApigatewayIntegrationAsync;
+
+    // The optional lambda to actualy invoke when this operation is invoked.
+    lambda?: aws.lambda.Function;
 }
 
 interface SwaggerOperation {
@@ -253,34 +279,167 @@ interface ApigatewayIntegrationAsync extends ApigatewayIntegrationBase {
     connectionId?: pulumi.Output<string>;
 }
 
-function swaggerSpecFromRoutes(name: string, routes: Route[]): [SwaggerSpec, {[key: string]: aws.lambda.Function}] {
-    const swagger: SwaggerSpec = createBaseSpec(name);
-    const lambdas: {[key: string]: aws.lambda.Function} = registerRoutes(this, name, routes, swagger);
+export function createSwaggerSpec(
+    name: string, routes?: Route[], staticRoutes?: StaticRoute[], opts?: pulumi.ResourceOptions): SwaggerSpec {
 
-    return [swagger, lambdas];
+    return addStaticRoutesToSwaggerSpec(
+        name, staticRoutes,
+        addRoutesToSwaggerSpec(routes, createBaseSwaggerSpec(name)),
+        opts);
 }
 
-function createBaseSpec(apiName: string): SwaggerSpec {
+export function addRoutesToSwaggerSpec(routes: Route[] | undefined, swagger: SwaggerSpec): SwaggerSpec {
+    if (routes) {
+        for (const route of routes) {
+            const method = swaggerMethod(route.method);
+            const lambda = createLambdaFunction(name + sha1hash(method + ":" + route.path), route.handler);
+            if (!swagger.paths[route.path]) {
+                swagger.paths[route.path] = {};
+            }
+            swagger.paths[route.path][method] = createPathSpecLambda(lambda);
+        }
+    }
+
+    return swagger;
+}
+
+export function addStaticRoutesToSwaggerSpec(
+    name: string, routes: StaticRoute[] | undefined, swagger: SwaggerSpec, opts?: pulumi.ResourceOptions): SwaggerSpec {
+
+    // If there are no static files or directories, then we can bail out early.
+    if (!routes || routes.length === 0) {
+        return swagger;
+    }
+
+    const method = swaggerMethod("GET");
+
+    // Create a bucket to place all the static data under.
+    const bucket = new aws.s3.Bucket(safeS3BucketName(name), undefined, opts);
+
+    function createRole(key: string) {
+        // Create a role and attach it so that this route can access the AWS bucket.
+        const role = new aws.iam.Role(key, {
+            assumeRolePolicy: JSON.stringify(apigatewayAssumeRolePolicyDocument),
+        }, opts);
+        const attachment = new aws.iam.RolePolicyAttachment(key, {
+            role: role,
+            policyArn: aws.iam.AmazonS3FullAccess,
+        }, opts);
+
+        return role;
+    }
+
+    // For each static file, just make a simple bucket object to hold it, and create a swagger
+    // path that routes from the file path to the arn for the bucket object.
+    //
+    // For static directories, use greedy api-gateway path matching so that we can map a single
+    // api gateway route to all the s3 bucket objects we create for the files in these
+    // directories.
+    for (const route of routes) {
+        const stat = fs.statSync(route.localPath);
+        if (stat.isFile()) {
+            processFile(route);
+        }
+        else if (stat.isDirectory()) {
+            processDirectory(route);
+        }
+    }
+
+    return swagger;
+
+    function createBucketObject(key: string, localPath: string, contentType?: string) {
+        const obj = new aws.s3.BucketObject(key, {
+            bucket: bucket,
+            key: key,
+            source: new pulumi.asset.FileAsset(localPath),
+            contentType: contentType || mime.getType(localPath) || undefined,
+        }, opts);
+    }
+
+    function processFile(route: StaticRoute) {
+        const key = name + sha1hash(method + ":" + route.path);
+        const role = createRole(key);
+
+        createBucketObject(key, route.localPath, route.contentType);
+
+        const pathSpec = createPathSpecObject(bucket, key, role);
+        swagger.paths[route.path] = { [method]: pathSpec };
+    }
+
+    function processDirectory(directory: StaticRoute) {
+        const directoryServerPath = directory.path.endsWith("/")
+            ? directory.path
+            : directory.path + "/";
+
+        const directoryKey = name + sha1hash(method + ":" + directoryServerPath);
+        const role = createRole(directoryKey);
+
+        let startDir = directory.localPath.startsWith("/")
+            ? directory.localPath
+            : fspath.join(process.cwd(), directory.localPath);
+
+        if (!startDir.endsWith(fspath.sep)) {
+            startDir = fspath.join(startDir, fspath.sep);
+        }
+
+        // If the user has supplied 'false' for options.index, then no speciam index file served
+        // at the root. Otherwise if they've supplied an actual filename to serve as the index
+        // file then use what they've provided.  Otherwise attempt to serve "index.html" at the
+        // root (if it exists).
+        const indexFile = directory.index === false
+            ? undefined
+            : typeof directory.index === "string"
+                ? directory.index
+                : "index.html";
+
+        const indexPath = indexFile === undefined ? undefined : fspath.join(startDir, indexFile);
+
+        // Recursively walk the directory provided, creating bucket objects for all the files we
+        // encounter.
+        function walk(dir: string) {
+            const children = fs.readdirSync(dir);
+
+            for (const childName of children) {
+                const childPath = fspath.join(dir, childName);
+                const stats = fs.statSync(childPath);
+
+                if (stats.isDirectory()) {
+                    walk(childPath);
+                }
+                else if (stats.isFile()) {
+                    const childRelativePath = childPath.substr(startDir.length);
+                    const childUrn = directoryKey + "/" + childRelativePath;
+
+                    createBucketObject(childUrn, childPath);
+
+                    if (childPath === indexPath) {
+                        // We hit the file that we also want to serve as the index file. Create
+                        // a specific swagger path from the server root path to it.
+                        const indexPathSpec = createPathSpecObject(bucket, childUrn, role);
+                        swagger.paths[directoryServerPath] = { [method]: indexPathSpec };
+                    }
+                }
+            }
+        }
+
+        walk(startDir);
+
+        // Take whatever path the client wants to host this folder at, and add the
+        // greedy matching predicate to the end.
+
+        const swaggerPath = directoryServerPath + "{proxy+}";
+        const pathSpec = createPathSpecObject(bucket, directoryKey, role, "proxy");
+        swagger.paths[swaggerPath] = { [swaggerMethod("any")]: pathSpec };
+    }
+}
+
+function createBaseSwaggerSpec(apiName: string): SwaggerSpec {
     return {
         swagger: "2.0",
         info: { title: apiName, version: "1.0" },
         paths: {},
-        "x-amazon-apigateway-binary-media-types": [ "*/*" ],
+        "x-amazon-apigateway-binary-media-types": ["*/*"],
     };
-}
-
-function registerRoutes(parent: pulumi.Resource, apiName: string, routes: Route[], swagger: SwaggerSpec): {[key: string]: aws.lambda.Function} {
-    const lambdas: {[key: string]: aws.lambda.Function} = {};
-    for (const route of routes) {
-        const method: string = swaggerMethod(route.method);
-        const lambda = createLambdaFunction(apiName + sha1hash(method + ":" + route.path), route.handler);
-        lambdas[method + ":" + route.path] = lambda;
-        if (!swagger.paths[route.path]) {
-            swagger.paths[route.path] = {};
-        }
-        swagger.paths[route.path][method] = createPathSpecLambda(lambda);
-    }
-    return lambdas;
 }
 
 function swaggerMethod(method: string): string {
@@ -312,6 +471,7 @@ function createPathSpecLambda(lambda: aws.lambda.Function): SwaggerOperationAsyn
             httpMethod: "POST",
             type: "aws_proxy",
         },
+        lambda: lambda,
     };
 }
 
@@ -367,3 +527,88 @@ function createSwaggerString(spec: SwaggerSpec): pulumi.Output<string> {
     }
 }
 
+function createPathSpecObject(
+        bucket: aws.s3.Bucket,
+        key: string,
+        role: aws.iam.Role,
+        pathParameter?: string): SwaggerOperationAsync {
+
+    const region = aws.config.requireRegion();
+
+    const uri = bucket.bucket.apply(bucketName =>
+        `arn:aws:apigateway:${region}:s3:path/${bucketName}/${key}${(pathParameter ? `/{${pathParameter}}` : ``)}`);
+
+    const result: SwaggerOperationAsync = {
+        responses: {
+            "200": {
+                description: "200 response",
+                schema: { type: "object" },
+                headers: {
+                    "Content-Type": { type: "string" },
+                    "content-type": { type: "string" },
+                },
+            },
+            "400": {
+                description: "400 response",
+            },
+            "500": {
+                description: "500 response",
+            },
+        },
+        "x-amazon-apigateway-integration": {
+            credentials: role.arn,
+            uri: uri,
+            passthroughBehavior: "when_no_match",
+            httpMethod: "GET",
+            type: "aws",
+            responses: {
+                "4\\d{2}": {
+                    statusCode: "400",
+                },
+                "default": {
+                    statusCode: "200",
+                    responseParameters: {
+                        "method.response.header.Content-Type": "integration.response.header.Content-Type",
+                        "method.response.header.content-type": "integration.response.header.content-type",
+                    },
+                },
+                "5\\d{2}": {
+                    statusCode: "500",
+                },
+            },
+        },
+    };
+
+    if (pathParameter) {
+        result.parameters = [{
+            name: pathParameter,
+            in: "path",
+            required: true,
+            type: "string",
+        }];
+
+        result["x-amazon-apigateway-integration"].requestParameters = {
+            [`integration.request.path.${pathParameter}`]: `method.request.path.${pathParameter}`,
+        };
+    }
+
+    return result;
+}
+
+const apigatewayAssumeRolePolicyDocument = {
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "",
+            "Effect": "Allow",
+            "Principal": {
+                "Service": "apigateway.amazonaws.com",
+            },
+            "Action": "sts:AssumeRole",
+        },
+    ],
+};
+
+function safeS3BucketName(apiName: string): string {
+    return apiName.toLowerCase().replace(/[^a-z0-9\-]/g, "");
+}

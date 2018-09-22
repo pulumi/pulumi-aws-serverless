0d3ec1a37eb7ae15af074f90f06acbf771830d0e
Cyrus Najmabadi <cyrus@pulumi.com>
2018-09-21 19:54:44 -0700

    Move aws code down.

--- nodejs/aws-serverless/cloudwatch.ts
+++ nodejs/aws-serverless/cloudwatch.ts
@@ -12,68 +12,32 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import * as aws from "@pulumi/aws";
-import { cloudwatch, lambda } from "@pulumi/aws";
 import * as pulumi from "@pulumi/pulumi";
-import { createLambdaFunction, Handler } from "./function";
-import { EventSubscription } from "./subscription";
 
-export interface CloudwatchEventArgs {
-}
+import { cloudwatch } from "@pulumi/aws";
 
-export interface CloudwatchEvent {
-    account: string;
-    region: string;
-    detail: any;
-    "detail-type": string;
-    source: string;
-    time: string;
-    id: string;
-    resources: string[];
-}
-
-export type CloudwatchEventHandler = Handler<CloudwatchEvent, void>;
-
+/** @deprecated Use [cloudwatch.EventRuleEventSubscriptionArgs] instead */
+export type CloudwatchEventArgs = cloudwatch.EventRuleEventSubscriptionArgs;
+/** @deprecated Use [cloudwatch.EventRuleEvent] instead */
+export type CloudwatchEvent = cloudwatch.EventRuleEvent;
+/** @deprecated Use [cloudwatch.EventRuleEventHandler] instead */
+export type CloudwatchEventHandler = cloudwatch.EventRuleEventHandler;
+/** @deprecated Use [cloudwatch.onSubscribe] or [cloudwatch.EventRule.onEvent] instead */
 export function onEvent(name: string, schedule: string, handler: CloudwatchEventHandler, args?: CloudwatchEventArgs, opts?: pulumi.ResourceOptions): CloudwatchEventSubscription;
 export function onEvent(name: string, rule: cloudwatch.EventRule, handler: CloudwatchEventHandler, args?: CloudwatchEventArgs, opts?: pulumi.ResourceOptions): CloudwatchEventSubscription;
 export function onEvent(
     name: string, scheduleOrRule: string | cloudwatch.EventRule,
     handler: CloudwatchEventHandler, args?: CloudwatchEventArgs, opts?: pulumi.ResourceOptions): CloudwatchEventSubscription {
 
-    args = args || {};
-    const func = createLambdaFunction(name + "-cloudwatch-subscription", handler, opts);
-    return new CloudwatchEventSubscription(name, scheduleOrRule, func, args, opts);
-}
-
-export class CloudwatchEventSubscription extends EventSubscription {
-    public readonly eventRule: cloudwatch.EventRule;
-    public readonly target: cloudwatch.EventTarget;
-
-    public constructor(
-        name: string, scheduleOrRule: string | cloudwatch.EventRule, func: lambda.Function,
-        args: CloudwatchEventArgs, opts?: pulumi.ResourceOptions) {
-
-        super("aws-serverless:cloudwatch:CloudwatchEventSubscription", name, func, { }, opts);
-
-        if (typeof scheduleOrRule === "string") {
-            this.eventRule = new cloudwatch.EventRule(name, {
-                scheduleExpression: scheduleOrRule,
-            }, { parent: this });
-        } else {
-            this.eventRule = scheduleOrRule;
-        }
-
-        this.target = new aws.cloudwatch.EventTarget(name, {
-            rule: this.eventRule.name,
-            arn: this.func.arn,
-            targetId: name,
-        }, { parent: this });
-
-        this.permission = new aws.lambda.Permission(name, {
-            action: "lambda:invokeFunction",
-            function: this.func,
-            principal: "events.amazonaws.com",
-            sourceArn: this.eventRule.arn,
-        }, { parent: this });
+    if (typeof scheduleOrRule === "string") {
+        return cloudwatch.onSchedule(name, scheduleOrRule, handler, args, opts);
     }
+
+    return scheduleOrRule.onEvent(name, handler, args, opts);
 }
+
+/** @deprecated Use [cloudwatch.EventRuleEventSubscription] instead */
+// tslint:disable-next-line:variable-name
+export const CloudwatchEventSubscription = cloudwatch.EventRuleEventSubscription;
+/** @deprecated Use [cloudwatch.EventRuleEventSubscription] instead */
+export type CloudwatchEventSubscription = cloudwatch.EventRuleEventSubscription;
